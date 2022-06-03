aec62e80772d1ca3b2baa8b9211f524e1ab56310
gamejunkiez <queenangrylobster@gmail.com>
2022-06-03 13:45:33 -0500

    completed exercise

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyntaxSugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
+        }
+    }
+}
